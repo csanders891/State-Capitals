7698603fce01b4c3307fcf8f618ed0094532c8b0
Carter Sanders <65356624+csanders891@users.noreply.github.com>
2025-09-09 13:10:44 -0500

    Add files via upload

---
+++ script/StateCapitals.js
@@ -0,0 +1,247 @@
+function alabama()
+{
+    alert("Montgomery");
+}
+
+function alaska()
+{
+    alert("Juneau");
+}
+
+function arizona()
+{
+    alert("Phoenix");
+    
+}
+
+function arkansas()
+{
+    alert("Little Rock");
+}
+
+function california()
+{
+    alert("Sacramento");
+}
+
+function colorado()
+{
+    alert("Denver");
+}
+
+function connecticut()
+{
+    alert("Hartford");
+}
+function deleware()
+{
+    alert("Dover");
+}
+
+function florida()
+{
+    alert("Tallahassee");
+}
+
+function georgia()
+{
+    alert("Atlanta");
+}
+function hawaii()
+{
+    alert("Honolulu");
+}
+
+function idaho()
+{
+    alert("Boise");
+}
+
+function illinois()
+{
+    alert("Springfield");
+}
+
+function indiana()
+{
+    alert("Indianapolis");
+}
+
+function iowa()
+{
+    alert("Des Moines");
+}
+
+function kansas()
+{
+    alert("Topeka");
+}
+
+function kentucky()
+{
+    alert("Frankfort");
+}
+
+function louisiana()
+{
+    alert("Baton Rouge");
+}
+
+function maine()
+{
+    alert("Augusta");
+}
+
+function maryland()
+{
+    alert("Annapolis");
+}
+
+function massachusetts()
+{
+    alert("Boston");
+}
+
+function michigan()
+{
+    alert("Lansing");
+}
+
+function minnesota()
+{
+    alert("St. Paul");
+}
+
+function mississippi()
+{
+    alert("Jackson");
+}
+
+function missouri()
+{
+    alert("Jefferson City");
+}
+
+function montana()
+{
+    alert("Helena");
+}
+
+function nebraska()
+{
+    alert("Lincoln");
+}
+
+function nevada()
+{
+    alert("Carson City");
+}
+
+function newHampshire()
+{
+    alert("Concord");
+}
+
+function newJersey()
+{
+    alert("Trenton");
+}
+
+function newMexico()
+{
+    alert("Santa Fe");
+}
+
+function newYork()
+{
+    alert("Albany");
+}
+
+function northCarolina()
+{
+    alert("Raleigh");
+}
+
+function northDakota()
+{
+    alert("Bismark");
+}
+
+function ohio()
+{
+    alert("Columbus");
+}
+
+function oklahoma()
+{
+    alert("Oklahoma City");
+}
+
+function oregon()
+{
+    alert("Salem");
+}
+function pennsylvania()
+{
+    alert("Harrisburg");
+}
+
+function rhodeIsland()
+{
+    alert("Providence");
+}
+
+function southCarolina()
+{
+    alert("Columbia");
+}
+
+function southDakota()
+{
+    alert("Pierre");
+}
+
+function tennessee()
+{
+  alert("Nashville");  
+}
+
+function texas()
+{
+    alert("Austin");   
+}
+
+function utah()
+{
+    alert("Salt Lake City");
+}
+
+function vermont()
+{
+    alert("Montpelier");
+}
+
+function virginia()
+{
+    alert("Richmond");
+}
+
+function washington()
+{
+    alert("Olympia");
+}
+
+function westVirginia()
+{
+    alert("Charleston");
+}
+
+function wisconsin()
+{
+    alert("Madison");
+}
+
+function wyoming()
+{
+    alert("Cheyenne");
+}
